2f894ed8187052982cb79e4ea303d1922a86b6d1
josef <josef@LAPTOP-3RVOAQTN>
2018-10-23 14:39:23 +0200

    Kategorilayout

--- IvanochJoseftest/Form1.Designer.cs
+++ IvanochJoseftest/Form1.Designer.cs
@@ -33,14 +33,18 @@
             this.lbKategori = new System.Windows.Forms.ListBox();
             this.listView1 = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnNyKategori = new System.Windows.Forms.Button();
+            this.btnSparaKategori = new System.Windows.Forms.Button();
+            this.btnTaBortKategori = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(673, 339);
+            this.button2.Location = new System.Drawing.Point(598, 271);
             this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(115, 74);
+            this.button2.Size = new System.Drawing.Size(102, 59);
             this.button2.TabIndex = 0;
             this.button2.Text = "Click For Dick";
             this.button2.UseVisualStyleBackColor = true;
@@ -49,9 +53,9 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 16);
+            this.label1.Location = new System.Drawing.Point(730, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(86, 20);
+            this.label1.Size = new System.Drawing.Size(78, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Kategorier:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -59,20 +63,20 @@
             // lbKategori
             // 
             this.lbKategori.FormattingEnabled = true;
-            this.lbKategori.ItemHeight = 20;
-            this.lbKategori.Location = new System.Drawing.Point(18, 52);
-            this.lbKategori.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.lbKategori.ItemHeight = 16;
+            this.lbKategori.Location = new System.Drawing.Point(733, 29);
             this.lbKategori.Name = "lbKategori";
-            this.lbKategori.Size = new System.Drawing.Size(134, 104);
+            this.lbKategori.Size = new System.Drawing.Size(239, 164);
             this.lbKategori.TabIndex = 3;
             // 
             // listView1
             // 
             this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1});
-            this.listView1.Location = new System.Drawing.Point(195, 16);
+            this.listView1.Location = new System.Drawing.Point(173, 13);
+            this.listView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(593, 305);
+            this.listView1.Size = new System.Drawing.Size(528, 245);
             this.listView1.TabIndex = 4;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -82,11 +86,49 @@
             this.columnHeader1.Text = "Names";
             this.columnHeader1.Width = 285;
             // 
+            // btnNyKategori
+            // 
+            this.btnNyKategori.Location = new System.Drawing.Point(733, 228);
+            this.btnNyKategori.Name = "btnNyKategori";
+            this.btnNyKategori.Size = new System.Drawing.Size(75, 23);
+            this.btnNyKategori.TabIndex = 5;
+            this.btnNyKategori.Text = "Ny kategori";
+            this.btnNyKategori.UseVisualStyleBackColor = true;
+            // 
+            // btnSparaKategori
+            // 
+            this.btnSparaKategori.Location = new System.Drawing.Point(815, 227);
+            this.btnSparaKategori.Name = "btnSparaKategori";
+            this.btnSparaKategori.Size = new System.Drawing.Size(75, 23);
+            this.btnSparaKategori.TabIndex = 6;
+            this.btnSparaKategori.Text = "Spara";
+            this.btnSparaKategori.UseVisualStyleBackColor = true;
+            // 
+            // btnTaBortKategori
+            // 
+            this.btnTaBortKategori.Location = new System.Drawing.Point(897, 227);
+            this.btnTaBortKategori.Name = "btnTaBortKategori";
+            this.btnTaBortKategori.Size = new System.Drawing.Size(75, 23);
+            this.btnTaBortKategori.TabIndex = 7;
+            this.btnTaBortKategori.Text = "Ta bort";
+            this.btnTaBortKategori.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(733, 200);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(239, 22);
+            this.textBox1.TabIndex = 8;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(993, 533);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnTaBortKategori);
+            this.Controls.Add(this.btnSparaKategori);
+            this.Controls.Add(this.btnNyKategori);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.lbKategori);
             this.Controls.Add(this.label1);
@@ -108,6 +150,10 @@
         private System.Windows.Forms.ListBox lbKategori;
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.Button btnNyKategori;
+        private System.Windows.Forms.Button btnSparaKategori;
+        private System.Windows.Forms.Button btnTaBortKategori;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
