14b67c0ddd54b98c6586e3a1fa212bf9b9c3c886
stiligafigurer <35599659+stiligafigurer@users.noreply.github.com>
2018-10-23 13:42:52 +0200

    MASSIVE COMMIT

--- IvanochJoseftest/Form1.Designer.cs
+++ IvanochJoseftest/Form1.Designer.cs
@@ -29,11 +29,13 @@
         private void InitializeComponent()
         {
             this.button2 = new System.Windows.Forms.Button();
+            this.ListView1 = new System.Windows.Forms.ListView();
+            this.Namn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(349, 161);
+            this.button2.Location = new System.Drawing.Point(661, 324);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(115, 74);
             this.button2.TabIndex = 0;
@@ -41,11 +43,27 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // ListView1
+            // 
+            this.ListView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Namn});
+            this.ListView1.Location = new System.Drawing.Point(22, 13);
+            this.ListView1.Name = "ListView1";
+            this.ListView1.Size = new System.Drawing.Size(716, 268);
+            this.ListView1.TabIndex = 1;
+            this.ListView1.UseCompatibleStateImageBehavior = false;
+            this.ListView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Namn
+            // 
+            this.Namn.Text = "Namn";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ListView1);
             this.Controls.Add(this.button2);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +75,8 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView ListView1;
+        private System.Windows.Forms.ColumnHeader Namn;
     }
 }
 
--- IvanochJoseftest/Form1.cs
+++ IvanochJoseftest/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.ServiceModel.Syndication;
+using System.Xml;
 
 namespace IvanochJoseftest
 {
@@ -15,11 +17,26 @@ namespace IvanochJoseftest
         public Form1()
         {
             InitializeComponent();
+            hej();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             button2.Text = "B=======D";
         }
+
+        public void hej()
+        {
+            string url = @"https://cdn.radioplay.se/data/rss/490.xml";
+            XmlReader reader = XmlReader.Create(url);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            reader.Close();
+            foreach(SyndicationItem item in feed.Items)
+            {
+                string namn = item.Title.Text;
+                ListView1.Items.Add(namn);
+            }
+        }
+
     }
 }
